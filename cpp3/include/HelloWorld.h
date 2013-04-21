66a025fca427653e8a379d1fb971fa1ada9fef6a
Marie Piolot <mariepiolot@gmail.com>
2013-04-21 21:41:59 +0200

    ajout du Doxyfile

--- cpp3/include/HelloWorld.h
+++ cpp3/include/HelloWorld.h
@@ -1,7 +1,7 @@
 #ifndef __HELLOWORLD_H__
 #define __HELLOWORLD_H__
 
-/*! prints "hello world" to the standard output */
+/*! holds a Do method that prints "hello world" to the standard output */
 class HelloWorld
 {
     public:
