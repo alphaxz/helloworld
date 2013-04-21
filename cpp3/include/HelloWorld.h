96dbc1c17d874730003c3760e861388fc3e8be20
Marie Piolot <mariepiolot@gmail.com>
2013-04-21 19:22:38 +0200

    nouveau projet en CPP : classe HelloWorld

---
+++ cpp3/include/HelloWorld.h
@@ -0,0 +1,11 @@
+#ifndef __HELLOWORLD_H__
+#define __HELLOWORLD_H__
+
+/*! prints "hello world" to the standard output */
+class HelloWorld
+{
+    /*! prints "hello world" to the standard output */
+    void Do (void);
+};
+
+#endif /* __HELLOWORLD_H__ */
---
+++ cpp3/source/main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "HelloWorld.h"
+
+int main (int /* argc */, char* /* argv */[])
+{
+    HelloWorld hw;
+    hw.Do();
+}
